b0c8a578b558ca64723753f10e5bf3b0d0e30e79
Hatren37 <fred.velfords@gmail.com>
2025-06-18 11:59:53 +0300

    Added a newline at the end of the file for better formatting

--- file handling(json).py
+++ file handling(json).py
@@ -9,6 +9,7 @@ tracy = {
 file_path = "Tr.json"
 with open(file_path, "w") as file:
     json.dump(tracy, file) # Writing the dictionary to a JSON file
+    
 with open(file_path, "r") as file:
     data = json.load(file)  # Reading the JSON file back into a dictionary
     print(data)  # Output: {'name': 'Namukwaya Tracy', 'age': 30, 'city': 'kampala'}
