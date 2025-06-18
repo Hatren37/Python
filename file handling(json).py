ee72eb3446f36bc5ad510f2cbe798d351a3e8cfd
Hatren37 <fred.velfords@gmail.com>
2025-06-18 11:29:08 +0300

    Added an assignment on writing and reading files

---
+++ file handling(json).py
@@ -0,0 +1,14 @@
+import json
+
+tracy = {
+    "name": "Namukwaya Tracy",
+    "age": 30,
+    "city": "kampala",
+}
+
+file_path = "Tr.json"
+with open(file_path, "w") as file:
+    json.dump(tracy, file) # Writing the dictionary to a JSON file
+with open(file_path, "r") as file:
+    data = json.load(file)  # Reading the JSON file back into a dictionary
+    print(data)  # Output: {'name': 'Namukwaya Tracy', 'age': 30, 'city': 'kampala'}
